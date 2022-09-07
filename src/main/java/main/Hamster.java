024ae1abe3b84b260c3822bf6ac8dde7748c4942
martin <mmarquez210@gmail.com>
2022-09-07 18:43:53 -0300

    actualizamos funcionalidad agregando la clase Hamster

---
+++ src/main/java/main/Hamster.java
@@ -0,0 +1,16 @@
+package main;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Hamster extends Animal{
+
+    private String atributoHamster;
+
+    public Hamster(String name, String atributoHamster) {
+        super(name);
+        this.atributoHamster = atributoHamster;
+    }
+}
