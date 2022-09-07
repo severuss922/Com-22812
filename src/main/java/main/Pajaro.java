e84434c500b4afd8c4ce2b2155db7bfb22853fa9
Marcos Daniel Saguier <matecondani@gmail.com>
2022-09-07 19:27:24 -0300

    nivel de pajaro agregado

--- src/main/java/main/Pajaro.java
+++ src/main/java/main/Pajaro.java
@@ -9,8 +9,19 @@ public class Pajaro extends Animal{
 
     private String attrPajaro;
 
+    private int nivel;
+
     public Pajaro(String name, String attrPajaro) {
         super(name);
         this.attrPajaro = attrPajaro;
+    }
+
+    @Override
+    public String toString() {
+        return "Pajaro{" +
+                "name='" + name + '\'' +
+                ", attrPajaro='" + attrPajaro + '\'' +
+                ", nivel=" + nivel +
+                '}';
     }
 }
